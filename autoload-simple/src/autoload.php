9d9c1c9e51d9d6ab4b0702e8068d9083e607f854
Omar <omar.veyron@gmail.com>
2020-10-25 16:22:02 +0800

    :pencil: Write comments.

--- autoload-simple/src/autoload.php
+++ autoload-simple/src/autoload.php
@@ -1,9 +1,11 @@
 <?php
 spl_autoload_register(function ($className) {
     // Rectify $className to match the file location for that class.
+    // Example: Convert MyProject\API\Auth -> src/API/Auth
     $className = str_replace(["\\", "MyProject"], ["/", "src"], $className);
-    $filename  =  ABSPATH . "/$className.php";
+    $filename  =  ABSPATH . "/$className.php"; // e,g: /var/www/src/API/Auth.php
 
+    // Check if file exists before requiring it.
     if (file_exists($filename)) {
         require($filename);
     }

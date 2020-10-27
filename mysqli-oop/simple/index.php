b175396da2fbb785622c133d1475033ecaa90b21
Omar <omar.veyron@gmail.com>
2020-10-27 16:50:29 +0800

    :sparkles: Add MySQLi simple OOP style.

---
+++ mysqli-oop/simple/index.php
@@ -0,0 +1,18 @@
+<?php
+$conn = new mysqli("localhost", "root", "p455w0rD", "kbtest");
+if ($conn->connect_errno) {
+    printf(
+        '<h1>%s</h1>Error: %s',
+        "Error connecting to the database.",
+        $conn->connect_error
+    );
+    exit;
+}
+
+$result = $conn->query("SELECT name FROM sm_user");
+if ($result !== false) {
+    while ($row = $result->fetch_assoc()) {
+        echo "{$row['name']}<br/>";
+    }
+    $result->free_result();
+}
